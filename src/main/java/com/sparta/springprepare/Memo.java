b8e91348cc73745b4194e12c7975e93eacef0589
SeungBum Baek <wer06099@naver.com>
2023-06-12 14:24:36 +0900

    application, lombok

---
+++ src/main/java/com/sparta/springprepare/Memo.java
@@ -0,0 +1,9 @@
+package com.sparta.springprepare;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Memo {
+    private final String username;
+    private String contents;
+}
